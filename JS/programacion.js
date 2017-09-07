3d282a9fa65e9c56fadaf89aed72a2c0f83be44b
Allen Kevin Zapien Zamora <yosoy@arkev.com>
2017-09-06 22:44:36 -0500

    arreglos varios
    en la pantalla de gracias agregar un botón y arreglar el footer en
    dispositivos móviles
    
    arreglar el botón de CT2 para que pusiera el foco en el primer input al
    momento de subir.
    
    modificar las imágenes de redes sociales

--- JS/programacion.js
+++ JS/programacion.js
@@ -4,12 +4,42 @@ $(document).ready(function () {
 });
 
 //ir a arriba al dar click en la segunda llamada a la acción
-$('.irPaRriba').click(function go2Top() {
-	$("html, body").animate({
-		scrollTop: 0
-	}, 600);
-	return false;
-});
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 
 //Ir a Twitter
 $(".twitter").on('click', function () {
@@ -25,8 +55,6 @@ $(".facebook").on('click', function () {
 $(".whatsapp").on('click', function () {
 	window.location = "https://api.whatsapp.com/send?phone=5218126681066";
 });
-
-
 
 //Formulario de Agile
 (function (a) {
